7faee9af6ce850267b631c8a53964205fa8b6c07
suraiyasultana826 <suraiyasultana826@gmail.com>
2025-03-19 19:30:51 +0600

    ...

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express'
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello devs!')
+})
+
+ app.post('/', (req: Request, res: Response)=> {
+  console.log(req.body);
+  res.send('got data');
+ })
+
+export default app;
